020e5255c87f33857a3e6be89644025c9ca5a8db
Nethaji <nethaji4172@gmail.com>
2023-09-26 18:15:47 +0430

    Navigation functionally completed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">This is next JS</h1>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div></div>;
 }
